04bb826f1d1affe2efbdcf2da79d8f55f12f83e4
SergeyTsVL <tsarskiytsarskiy@mail.ru>
2025-01-22 15:37:47 +0700

    3

--- cafe_order_system/orders/models.py
+++ cafe_order_system/orders/models.py
@@ -13,6 +13,7 @@ class Order(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='В ожидании', blank=True, null=True)
+    revenue = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return f"Status {self.status} for table {self.table_number}"
--- cafe_order_system/orders/views.py
+++ cafe_order_system/orders/views.py
@@ -10,6 +10,8 @@ from .forms import SignUpForm
 from django.contrib.auth import login, authenticate, logout
 
 
+REVENUE = 0   # Выручка за сессию
+
 def logout_view(request):
     """
     Этот метод выполняет выход пользователя из системы и перенаправляет его на домашнюю страницу.
@@ -43,36 +45,12 @@ def create_order(request):
         form = OrderForm(request.POST)
         if (form.is_valid() and form['table_number'].value() != '' and form['description'].value() != ''
                 and form['name'].value() != ''):
+            # print(form['description'].value())
             order = form.save()
             return redirect('order_detail', order.id)
     else:
         form = OrderForm()
     return render(request, 'orders/order_form.html', {'form': form})
-    # if request.method == "POST":
-
-
-
-# def string_to_nested_dict(text):
-#     parts = text.split('\n')
-#     dictionaries = []
-#     for part in parts:
-#         if ';' in part:
-#             inner_dict = dict(pair.split('=') for pair in part.split(';'))
-#             dictionaries.append(inner_dict)
-#         else:
-#             dictionaries.append({part.split('=')[0]: part.split('=')[1]})
-#
-#     return {i: inner_dict for i, inner_dict in enumerate(dictionaries)}
-#
-#
-# # Пример использования
-# text = "key1=value1;key2=value2\nkeyA=valueA\nkeyB=valueB"
-# result = string_to_nested_dict(text)
-# print(result)
-
-
-
-
 
 @login_required
 def order_editing(request, pk):
@@ -82,6 +60,25 @@ def order_editing(request, pk):
         if form.is_valid():
             order.name = request.user
             form.save()
+            print(type(order.status))
+        if order.status == 'Оплачено':
+            parts = order.description.split('\n')
+            # print(parts)
+            dictionaries = []
+            for part in parts:
+                dictionaries.append(part.split('-'))
+            try:
+                for i in dictionaries:
+                    print(i, 1111111111111)
+                    cleaned_list = [item.replace('\r', '') for item in i]
+                    print(cleaned_list, 2222222222222222222)
+                    # global REVENUE
+                    order.revenue += int(cleaned_list[1]) * int(cleaned_list[2])
+                    order.revenue.save()
+                    # order.revenue = REVENUE
+                    print(order.revenue, "order_editing")
+            except:
+                print('Не корректное отображение выручки')
 
             return redirect('/search')
     else:
@@ -89,61 +86,6 @@ def order_editing(request, pk):
         form = OrderForm(instance=order)
     return render(request, 'orders/editing_order.html',
                   {'form': form, 'order': order})
-
-
-
-
-    # order = get_object_or_404(Order, id=order_id)
-    # if request.method == "POST":
-    #     form = StatusChangeForm(request.POST)
-    #     if form.is_valid():
-    #         order.status = form.cleaned_data['status']
-    #         order.save()
-    #         return redirect('order_list')
-    # else:
-    #     form = StatusChangeForm(initial={'status': order.status})
-    # return render(request, 'orders/status_change_form.html', {'form': form, 'order_id': order.id})
-
-# @login_required    # Проверяет регистрацию пользователя
-# def edit_advertisement(request, pk):
-
-#     advertisement = Advertisement.objects.get(pk=pk)
-#     if advertisement.status != 'товар передан в доставку' and advertisement.status != 'товар доставлен':
-#         if request.method == "POST":
-#             form = AdvertisementForm(request.POST, request.FILES, instance=advertisement)
-#
-#             if form.is_valid():
-#                 advertisement.name = request.user
-#
-#                 advertisement.status = 'заявка создана'
-#                 form.save()
-#                 img_obj = form.instance
-#                 # Перенаправляет на страницу с сохраненными исправлениями.
-#                 return redirect('board:advertisement_detail', pk=img_obj.pk)
-#         else:
-#             # вызов функции которая отобразит в браузере указанный шаблон с данными формы и объявления.
-#             form = AdvertisementForm(instance=advertisement)
-#         return render(request, 'board/edit_advertisement.html',
-#                       {'form': form, 'advertisement': advertisement})
-#     return redirect('board:advertisement_detail', pk=pk)
-
-
-
-
-
-# @login_required
-# def add_item_to_order(request, order_id):
-#     order = get_object_or_404(Order, id=order_id)
-#     if request.method == "POST":
-#         form = AddItemForm(request.POST)
-#         if form.is_valid():
-#             item = form.cleaned_data['item']
-#             quantity = form.cleaned_data['quantity']
-#             OrderItem.objects.create(order=order, item=item, quantity=quantity)
-#             return redirect('order_detail', order_id)
-#     else:
-#         form = AddItemForm()
-#     return render(request, 'orders/add_item_form.html', {'form': form, 'order_id': order_id})
 
 @login_required
 def delete_order(request, order_id):
@@ -153,22 +95,13 @@ def delete_order(request, order_id):
         return redirect('/search')
     return render(request, 'orders/delete_order_form.html', {'order': order})
 
-    # advertisement = Advertisement.objects.get(pk=pk)
-    # if advertisement.status != 'товар передан в доставку' and advertisement.status != 'товар доставлен':
-    #     if request.method == "POST":
-    #         advertisement.delete()
-    #         return redirect('board:advertisement_list')
-    #     else:
-    #         None
-    #     return render(request, 'board/delete_advertisement.html', {'advertisement': advertisement})
-    # else:
-    #     return redirect('board:advertisement_list')
-
 @login_required
-def search_orders(request, price=0):
+def search_orders(request, price=0, revenue_1=0):
     orders = Order.objects.all()
+    global REVENUE
     price_list = []
     for order in orders:
+        order.revenue = 0
         parts = order.description.split('\n')
         dictionaries = []
         for part in parts:
@@ -180,15 +113,27 @@ def search_orders(request, price=0):
         except:
             price = None
         order.price = price
-        price_list.append(order.price)
+        price_list.append(price)
         price = 0
+        if order.status == 'Оплачено':
+            parts = order.description.split('\n')
+            dictionaries = []
+            for part in parts:
+                dictionaries.append(part.split('-'))
+            try:
+                for i in dictionaries:
+                    cleaned_list = [item.replace('\r', '') for item in i]
+                    order.revenue += float(int(cleaned_list[1]) * int(cleaned_list[2]))
+                    revenue_1 += order.revenue
+            except:
+                print('Не корректное отображение выручки')
     try:
         total_price_list = sum(price_list)
     except:
         total_price_list = "не корректно введена цена"
 
     return render(request, 'orders/order_list.html', {'orders': orders, 'price_list': price_list,
-                                                      'total_price_list': total_price_list, 'price': price})
+                                          'total_price_list': total_price_list, 'price': price, 'REVENUE': revenue_1})
 
 @login_required
 def order_detail(request, order_id):
